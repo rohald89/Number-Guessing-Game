638acd82b17bcbe66d478df0b407537eae21d6b1
Rohald van Merode <rohald_89@live.nl>
2021-10-03 22:38:50 +0200

    improved messages

--- guessing_game.py
+++ guessing_game.py
@@ -14,6 +14,8 @@ print(f"""
 ╰╯╱╰━┻━━┻┻┻┻━━┻━━┻╯╱╰━━━┻━━┻━━┻━━┻━━┻┻╯╰┻━╮┃╰━━━┻╯╰┻┻┻┻━━┻╯
 ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━╯┃
 ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━━╯
+
+Try to guess the random number, can you beat the highscore?! 
 """)
 
 def start_game():
@@ -21,7 +23,7 @@ def start_game():
     random_number = random.randrange(1, 11)
     tries = 0
 
-    print(f'The current highscore is {highscore}, can you do better?')
+    print(f'The current highscore is {highscore}, can you beat that?\nPlease enter a number from 1 to 10')
     while True:
         try:
             chosen_number = int(input("> "))
@@ -34,16 +36,17 @@ def start_game():
         else:
             tries += 1
             if chosen_number < random_number:
-                print("It's higher")
+                print("It's higher, try again: ")
                 continue
             elif chosen_number > random_number:
-                print("It's lower")
+                print("It's lower, try again:")
                 continue
             elif chosen_number == random_number:
-                print(f"You win! It took you {tries} tries")
                 if highscore > tries:
-                    print("You set a new highscore!")
+                    print(f"You set a new highscore!\nIt only took you {tries} tries!\n")
                     highscore = tries
+                else:
+                    print(f"It took you {tries} tries. Try again to beat the highscore of {highscore}.")
                 break
     
     replay = input("Would you like to try again? Y/N ")
